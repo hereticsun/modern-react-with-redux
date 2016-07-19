4e9142d358a272ef2ff7eadf48ba62f01da34df0
Azlan Cuttilan <azlan@bynd.com>
2016-07-19 15:20:16 +0100

    TestUtils library
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,14 @@
 import psdom from 'jsdom';
 imporrt jquery from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 const $ = jquery(global.window);
+
+function renderComponent(ComponentClass) {
+    const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+
+    return $(ReactDOM.findDOMNode(componentInstance));
+}
