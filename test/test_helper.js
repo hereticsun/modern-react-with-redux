c246b0064d3dd541f9e796f6928ddf7a926267b9
Azlan Cuttilan <azlan@bynd.com>
2016-07-19 16:32:56 +0100

    Test helper review
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,11 +2,12 @@ import jsdom from 'jsdom';
 import jquery from 'jquery';
 import TestUtils from 'react-addons-test-utils';
 import ReactDOM from 'react-dom';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
+import chaiJquery from 'chai-jquery';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
@@ -23,7 +24,12 @@ function renderComponent(ComponentClass, props, state) {
 }
 
 $.fn.simulate = function(eventName, value) {
-    TestUtlis,Simulate[eventName](this[0]);
+    if(value) {
+        this.val(value);
+    }
+    TestUtils.Simulate[eventName](this[0]);
 }
+
+chaiJquery(chai, chai.util, $);
 
 export {renderComponent, expect };
