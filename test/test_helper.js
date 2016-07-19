c2bfdbb627890caf3263938b977a8aea7485658f
Azlan Cuttilan <azlan@bynd.com>
2016-07-19 15:29:48 +0100

    Defining a renderComponent
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,14 +1,25 @@
-import psdom from 'jsdom';
-imporrt jquery from 'jquery';
+import jsdom from 'jsdom';
+import jquery from 'jquery';
 import TestUtils from 'react-addons-test-utils';
 import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../src/reducers';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 const $ = jquery(global.window);
 
 function renderComponent(ComponentClass) {
-    const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+    const componentInstance = TestUtils.renderIntoDocument(
+        <Provider store={createStore(reducers)}>
+            <ComponentClass />
+        </Provider>
+    );
 
     return $(ReactDOM.findDOMNode(componentInstance));
 }
+
+export {renderComponent, expect };
