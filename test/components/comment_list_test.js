249f01b470a7ee648dd794b72449aba2790b7e1a
Azlan Cuttilan <azlan@bynd.com>
2016-07-19 11:44:44 +0100

    Expectations on content
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- test/components/comment_list_test.js
+++ test/components/comment_list_test.js
@@ -2,5 +2,19 @@ import { renderComponent, expect } from '../test_helper';
 import CommentList from '../../src/components/comment_list';
 
 describe('CommentList', () => {
+    let component;
 
+    beforeEach(() => {
+        const props = { comments: ['New comment', 'Other New Comment'] };
+        component = renderComponent(ComponentList, null, props);
+    });
+
+    it('shows an LI for each comment', () => {
+        expect(component.find('li').length).to.equal(2);
+    });
+
+    it('shows each comment that is provided', () => {
+        expect(component).to.contain('New Comment');
+        expect(component).to.contain('Other New Comment');
+    });
 });
