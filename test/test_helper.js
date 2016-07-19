a44b7dce25412102044e22a7b8cd50ba983cc57f
Azlan Cuttilan <azlan@bynd.com>
2016-07-19 15:11:01 +0100

    More JSDom setup
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,4 +1,6 @@
 import psdom from 'jsdom';
+imporrt jquery from 'jquery';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
+const $ = jquery(global.window);
