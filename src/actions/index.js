5178d31b65aa22770a7fe6f6c101ef479ca63909
Azlan Cuttilan <azlan@bynd.com>
2016-07-22 12:27:30 +0100

    Introducing redux thunk
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,15 @@
 
 
 export function signinUser({ email, password }) {
-    // Submit email password to server
+    return function(dispatch) {
+        // Submit email password to server
 
-    // If request is good:
-    // - Update state to indicate user is authenticated
-    // - Save the JWT token
-    // - redirect to the route '/feature'
+        // If request is good:
+        // - Update state to indicate user is authenticated
+        // - Save the JWT token
+        // - redirect to the route '/feature'
 
-    // If request is bad:
-    // - Show an error to the user
+        // If request is bad:
+        // - Show an error to the user
+    }
 }
