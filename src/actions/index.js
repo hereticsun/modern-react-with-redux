1da0dab800ef0beed0e747970783a7cf4b459a0a
Azlan Cuttilan <azlan@bynd.com>
2016-07-25 14:22:04 +0100

    Signup action creator
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- src/actions/index.js
+++ src/actions/index.js
@@ -25,6 +25,11 @@ export function signinUser({ email, password }) {
     }
 }
 
+export function signupUser({ email, password }) {
+    return function(dispatch) {
+        axios.post(`${ROOT_URL}/signup`, { email, password });
+    }
+}
 export function authError(error) {
     return {
         type: AUTH_ERROR,
